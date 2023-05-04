7653a8e73e4b918058e52b33e12ee1ca133226b9
David Renteria <david.j.renteria1@gmail.com>
2023-05-04 15:45:05 -0500

    updated

--- src/main/java/ShowAdsServlet.java
+++ src/main/java/ShowAdsServlet.java
@@ -5,14 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-
 @WebServlet(name="ShowAdsServlet", urlPatterns = "/ads")
 public class ShowAdsServlet extends HttpServlet {
         @Override
         protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
             // Use the factory to get the dao object
             Ads adsDao = DaoFactory.getAdsDao();
-            // Use a method on the dao to get all the products
+            // Use a method on the dao to get all the ads
             List<Ad> ads = adsDao.all();
             // Pass the data to the jsp
             request.setAttribute("ads", ads);
