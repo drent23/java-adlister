b9e356e926035070fed6fb3573a550d3fe3ffd02
David Renteria <david.j.renteria1@gmail.com>
2023-05-04 16:15:53 -0500

    still working

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
@@ -15,11 +16,19 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
-
         if (validAttempt) {
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
+        HttpSession session = request.getSession();
+        Object user = new Object();
+        session.setAttribute("user", user);
+        if ((boolean)request.getSession().getAttribute("user") == false) {
+            response.sendRedirect("/login");
+        } else {
+            response.sendRedirect("/profile");
+        } return;
+        request.getRequestDispatcher("/secret-admin-page.jsp").forward(request, response);
     }
 }
--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -10,4 +10,10 @@ public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        boolean isLoggedIn = true;
+        if (isLoggedIn) {
+            request.getSession().setAttribute("isLoggedIn", true);
+        }
+    }
 }
