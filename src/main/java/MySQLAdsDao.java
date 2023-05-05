a50c7f74d510be6ec33b0b44b4c433468867de59
David Renteria <david.j.renteria1@gmail.com>
2023-05-05 17:01:48 -0500

    still working pt 2

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,26 +1,35 @@
 import com.mysql.cj.jdbc.Driver;
 
+import javax.swing.plaf.nimbus.State;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 public class MySQLAdsDao implements Ads {
     private Connection connection;
     public MySQLAdsDao(Config config) {
-        try {
             DriverManager.registerDriver(new Driver());
             Connection connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/adlister_db?allowPublicKeyRetrieval=true&useSSL=false",
                     "root",
-                    "codeup"
-            );
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-        return ads;
+                    "codeup");
     }
     @Override
     public List<Ad> all() {
-        return null;
+        List<Ad> ads = new ArrayList();
+        String selectQuery = "SELECT * FROM ads";
+        try{
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/adlister_db?allowPublicKeyRetrieval=true&useSSL=false",
+                    "root",
+                    "codeup");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(selectQuery);
+            While (resultSet.next()) {
+                ads.add(new Ad(resultSet.getString("url"),resultSet.getString("username"), resultSet.getString("password"),))
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     @Override
     public Long insert(Ad ad) {
